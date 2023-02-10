ea5111269f40aae2f35e3eb8ed79001cac918e20
Innovation Services Peru <sergio.jn024@gmail.com>
2023-02-10 03:07:44 -0500

    Del

--- Formative_v19/Cuestionarios/C-Basico.js
+++
@@ -1,6 +0,0 @@
-function mensaje(form){
-alert("RESULTADOS DEL CUESTIONARIO NIVEL BASICO" +"...                 " +
- " ¡Felicidades Victor23 obtuviste 14 puntos!"
-);
-
-}
--- Formative_v19/js/niv-avanzado.js
+++
@@ -1,7 +0,0 @@
-// Javascript code
-function fijar(obj,pos) {
-	dObj=document.all(obj);
-	dObj.style.posTop=document.body.scrollTop+pos;
-}
-//Main function
-function 
